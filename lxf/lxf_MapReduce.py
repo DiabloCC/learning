1d85633b827dae345195b77b61ca1d86d4faa8eb
DiabloCC <chh986212@126.com>
2016-04-30 16:59:58 +0800

    MapReduce update 160430

--- lxf/lxf_MapReduce.py
+++ lxf/lxf_MapReduce.py
@@ -9,12 +9,31 @@ def normalize(s):
   if len(s)==1:
     return s[0].upper()
   return s[0].upper()+s[1:].lower()
+
+def prod(L):
+  def multi(a1, a2):
+    return a1 * a2
+  return reduce(multi, L)
+
+def str2float(s):
+  def mapf(a):
+    return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '.':}[a]
+  def reducef(a1, a2):
+    if a2 == '.':
+      return a1 / 10
+    else:
+      return a1 / 10 + a2 
   
 def main():
   # 测试:
+  print('-----map------')
   L1 = ['adam', 'LISA', 'barT']
   L2 = list(map(normalize, L1))
   print(L2)
-  
+  print('-----reduce------')
+  L1 = [3,5,7,9]
+  print('3*5*7*9 =',prod(L1))
+  print('-----map & reduce------')
+
 if __name__=='__main__':
   main()
